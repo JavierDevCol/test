a3e72081f5732dde314c4e0798d45a002793ea5b
javierdevcol <javiergarciadevcol@gmail.com>
2021-12-12 17:43:22 -0500

    asdasd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import DashBoard from './components/Dashboard/DashBoard';
+import Blog from './components/Home/Blog';
+import Login from './components/Login/Login';
+import SignUp from './components/Login/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Blog />} />
+          <Route exact path="/signUp" element={<SignUp />} />
+          <Route exact path="login" element={<Login />} />
+          <Route exact path="/dashboard" element={<DashBoard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
